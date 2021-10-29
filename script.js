335cc0de02868dfd74f6f69875b28ba85025dc95
GiulianoPrimavera <giuliano.plrimavera@gmail.com>
2021-10-29 10:04:16 +0200

    adds a message with vue using javascript, adds an image using vue

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+Vue.config.devtools = true;
+
+const vueApp = new Vue({
+    el: "#root",
+    data: {
+        message: "Ciao Amico",
+        source: "https://i.pinimg.com/564x/ba/0e/3b/ba0e3bb89a0918187ba855146142961f.jpg",
+        alternative: "immgaine",
+        textGreen: "text_green"
+    }
+})
